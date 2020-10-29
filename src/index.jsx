f4fcd93a1fdf8e6f7c087c6c921fbde7d79d7d1a
Alexey <alexey.shilin@me.com>
2020-10-29 16:36:28 +0300

    добавлен компонент

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Message from '../components/Message'
 
 console.log('Hello React!!!')
 
 class HelloMessage extends React.Component {
-    render() {
-      return (
-        <div>
-          Привет, {this.props.name}
-        </div>
-      );
-    }
+  constructor () {
+    super();
+
+    this.state = {
+      count: 0,
+      messages: [], 
+    };
+
+    this.onClick=this.onClick.bind(this)
+    this.addMessage=this.addMessage.bind(this)
+
   }
-  
-  ReactDOM.render(
-    <HelloMessage name="Алёщга" />,
-    document.getElementById('hello-example')
-  );
+
+  onClick() {
+    const{count} = this.state;
+    this.setState({count: count + 1 });
+  }
+
+  addMessage() {
+    const{messages, count} = this.state;
+    this.setState ({messages: [...messages, `новое сообщение ${count}`]})
+  }
+
+  render() {
+    const{count, messages} = this.state;
+
+    return (
+      <div> 
+        <h2>Привет, {this.props.name}</h2>
+        <p>{count}</p>
+        <button onClick={this.onClick}>увеличение</button>
+        <button onClick={this.addMessage }>Добавить сообщение</button>
+          <ul>
+          {messages.map((item, index) => (
+            <Message key={index} item={item} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<HelloMessage name="Алекс"/>,
+  document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].js'
   },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   module: {
     rules: [
       {
